4eb9b15af33596f2aba6e7664081631707147150
Brian Bansenauer <brianb@cascadia.edu>
2017-01-16 23:38:15 -0800

    added code for Customer View

---
+++ MVC-DataExploration/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_DataExploration.Models;
+
+namespace MVC_DataExploration.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private MyDbContext db = new MyDbContext();
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            Customer customer = new Customer();
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            return View("Index", db.Customers); 
+
+        }
+    
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(db.Customers);
+        }
+    }
+}
