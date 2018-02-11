f13a1ec573d44b43f8994f2caf42a06ff85d1fc8
bbruil <bjbrui@aol.com>
2018-02-11 08:48:07 -0800

    Database first

---
+++ MVC-DataExploration/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using MVC_DataExploration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_DataExploration.Controllers
+{
+    public class ProductController : Controller
+    {
+        MyDBContext db = new MyDBContext();
+        // GET: Product
+        public ActionResult Index()
+        {  
+            return View();
+        }
+        [HttpPost]
+            public ActionResult Index(Product p)
+        {
+            db.Products.Add(p);
+            db.SaveChanges();
+            return View();
+
+        }
+    }
+}
